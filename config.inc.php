3ec088251accf02582f21e7fc6c33a8c89dad9e7
Evan Sosenko <razorx@evansosenko.com>
2017-10-19 20:33:56 -0700

    Add Storage database tables to config

--- config.inc.php
+++ config.inc.php
@@ -14,3 +14,24 @@ $cfg['DefaultLang'] = 'en';
 $cfg['CaptchaLoginPublicKey'] = trim(getenv('PMA_CAPTCHA_PUBLIC_KEY'));
 $cfg['CaptchaLoginPrivateKey'] = trim(getenv('PMA_CAPTCHA_PRIVATE_KEY'));
 $cfg['blowfish_secret'] = trim(getenv('PMA_SECRET'));
+
+/* Storage database and tables */
+$cfg['Servers'][$i]['bookmarktable'] = 'pma__bookmark';
+$cfg['Servers'][$i]['relation'] = 'pma__relation';
+$cfg['Servers'][$i]['table_info'] = 'pma__table_info';
+$cfg['Servers'][$i]['table_coords'] = 'pma__table_coords';
+$cfg['Servers'][$i]['pdf_pages'] = 'pma__pdf_pages';
+$cfg['Servers'][$i]['column_info'] = 'pma__column_info';
+$cfg['Servers'][$i]['history'] = 'pma__history';
+$cfg['Servers'][$i]['table_uiprefs'] = 'pma__table_uiprefs';
+$cfg['Servers'][$i]['tracking'] = 'pma__tracking';
+$cfg['Servers'][$i]['userconfig'] = 'pma__userconfig';
+$cfg['Servers'][$i]['recent'] = 'pma__recent';
+$cfg['Servers'][$i]['favorite'] = 'pma__favorite';
+$cfg['Servers'][$i]['users'] = 'pma__users';
+$cfg['Servers'][$i]['usergroups'] = 'pma__usergroups';
+$cfg['Servers'][$i]['navigationhiding'] = 'pma__navigationhiding';
+$cfg['Servers'][$i]['savedsearches'] = 'pma__savedsearches';
+$cfg['Servers'][$i]['central_columns'] = 'pma__central_columns';
+$cfg['Servers'][$i]['designer_settings'] = 'pma__designer_settings';
+$cfg['Servers'][$i]['export_templates'] = 'pma__export_templates';
